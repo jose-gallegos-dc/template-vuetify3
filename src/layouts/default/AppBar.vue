ac85e6d144c58a5bf3d184090a22ab2ad8ca7051
jose-gallegos-dc <jose.gallegos.delcarpio@gmail.com>
2023-04-01 00:58:22 -0600

    first commit

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-circle-slice-4" />
+
+      Base Preset
+    </v-app-bar-title>
+
+    <div class="d-flex justify-space-around">
+      <v-btn to="/Test">Test</v-btn>
+    </div>
+
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
